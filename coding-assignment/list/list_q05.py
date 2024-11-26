1145e4f231964c5ee9b80e16ad7f23f72d5329c4
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-26 17:50:34 +0530

    git push origin main

---
+++ coding-assignment/list/list_q05.py
@@ -0,0 +1,5 @@
+# Given a list of numbers, write a Python program to print all positive numbers in given list. 
+lst=[-3,45,56,-37,40,22,-12,-46,24,-38]
+for i in lst:
+    if i>=0:
+        print( i, end=" ")
