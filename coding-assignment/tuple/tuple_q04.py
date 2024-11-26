ef222d6e2f6404cd81ba51dca2b5ec5828cd327d
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-26 19:58:32 +0530

    git push origin main

---
+++ coding-assignment/tuple/tuple_q04.py
@@ -0,0 +1,6 @@
+# Write a python program to get the 4th element and 4th element from last of a tuple.
+my_tuple = (10, 20, 30, 40, 50, 60, 70, 80)
+fourth_element = my_tuple[3]
+fourth_from_last = my_tuple[-4]
+print("4th element:", fourth_element)
+print("4th element from last:", fourth_from_last)
