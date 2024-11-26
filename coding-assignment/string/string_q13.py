dd71f7a1979cfec7a94709c70cda30ad2a9204e8
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-26 19:30:11 +0530

    git push origin main

---
+++ coding-assignment/string/string_q13.py
@@ -0,0 +1,6 @@
+# Problem: Write a function to find the position of the first occurrence of a character in a string.
+input_string = input()
+char = input()
+position = input_string.find(char)
+print(position)
+
