9e998086b361ddf78da6f2652dd17f5a5df14d07
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-26 19:12:32 +0530

    Create string_q11.py

---
+++ coding-assignment/string/string_q11.py
@@ -0,0 +1,7 @@
+# Problem: Write a function to check if a string starts with a given substring.
+input_string = input()
+substring = input()
+if input_string.startswith(substring):
+    print("Starts with the substring")
+else:
+    print("Does not start with the substring")
