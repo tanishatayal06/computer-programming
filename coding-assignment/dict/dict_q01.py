99f73444fc168d60820ff91057eb401e207f57a4
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-26 21:30:50 +0530

    git push origin main

--- coding-assignment/dict/dict_q01.py
+++ coding-assignment/dict/dict_q01.py
@@ -1,4 +1,4 @@
-Problem: Add a new key-value pair to the dictionary.
+# Problem: Add a new key-value pair to the dictionary.
 my_dict = {'name': 'John', 'age': 25}
 my_dict['city'] = 'New York'
 print(my_dict)
