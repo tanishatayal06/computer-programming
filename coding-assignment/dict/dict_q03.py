8957b081831b806c52c53365b44497077f5232fb
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-26 21:14:08 +0530

    git push origin main

---
+++ coding-assignment/dict/dict_q03.py
@@ -0,0 +1,6 @@
+# Problem: Check if a key exists in the dictionary
+my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
+if 'name' in my_dict:
+    print("Key exists")
+else:
+    print("Key does not exist")
