bcf61941f472fadcea431c2a5585f69f6d5853b3
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-26 20:56:18 +0530

    git push origin main

--- coding-assignment/tuple/tuple_q07.py
+++ coding-assignment/tuple/tuple_q07.py
@@ -1,4 +1,4 @@
-# Write a python program to remove an empty tuples from a list of tuples.
+# Write a python program to remove an empty tuples from a list of tuples
 my_list = [(10, 20), (), (30, 40), (), (50,)]
 filtered_list = [t for t in my_list if t]
 print("List after removing empty tuples:", filtered_list)
