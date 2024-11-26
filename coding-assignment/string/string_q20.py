43df7c92d59f81d3c0ac1f7eeca0a38c762d2224
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-26 19:43:42 +0530

    git push origin main

---
+++ coding-assignment/string/string_q20.py
@@ -0,0 +1,5 @@
+# Problem: Write a function to replace a substring with another string.
+input_string = input()
+old_substring = input()
+new_substring = input()
+print(input_string.replace(old_substring, new_substring))
