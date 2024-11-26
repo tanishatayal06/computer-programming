188c9457645aa1459cf869086b23d9e6d660441d
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-26 19:03:48 +0530

    git push origin main

---
+++ coding-assignment/string/string_q07.py
@@ -0,0 +1,6 @@
+# Problem: Write a function to check if a string contains only digits.
+input_string = input()
+if input_string.isdigit():
+    print("Only digits")
+else:
+    print("Contains non-digit characters")
