de6379f12289f79025f2f2b57b66f5ec318a5dfb
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-26 19:53:17 +0530

    git push origin main

---
+++ coding-assignment/tuple/tuple_q02.py
@@ -0,0 +1,8 @@
+# Write a python program to unpack a tuple in several variables.
+my_tuple = (10, 20, 30, 40, 50)
+a, b, c, d, e = my_tuple
+print("a:", a)
+print("b:", b)
+print("c:", c)
+print("d:", d)
+print("e:", e)
