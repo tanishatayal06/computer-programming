b75216f45b2c79f70132b28575be191dd8c9ca29
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-27 14:20:06 +0530

    git push origin main

---
+++ coding-assignment/list/list_q15.py
@@ -0,0 +1,7 @@
+# Problem: Write a Python program to multiply all the items in a list
+numbers = [1, 2, 3, 4, 5]
+result = 1 
+for item in numbers:
+    result *= item
+print("The product of all items in the list is:", result)
+
