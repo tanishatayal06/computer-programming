a3baa5085f94429208cbf134c823740426971a50
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-27 10:05:57 +0530

    git push origin main

---
+++ coding-assignment/function/function_q09.py
@@ -0,0 +1,26 @@
+# Problem: Write a function to find the number of triplets in a list that sum up to zero.
+def three_sum(nums):
+    nums.sort()
+    result = []
+
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+            if total < 0:
+                left += 1
+            elif total > 0:
+                right -= 1
+            else:
+                result.append([nums[i], nums[left], nums[right]])
+                left += 1
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+                right -= 1
+    return result
+
+# Test case
+nums = [-1, 0, 1, 2, -1, -4]
+print(three_sum(nums))  # Output: [[-1, -1, 2], [-1, 0, 1]]
