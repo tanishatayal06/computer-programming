00e7dd7c9019ceecc2553386d9b8244505e926e3
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-27 14:30:14 +0530

    git push origin main

---
+++ coding-assignment/list/list_q19.py
@@ -0,0 +1,5 @@
+# Problem: Write a Python program to get unique values from a list
+numbers = [1, 2, 2, 3, 4, 4, 5, 5, 6]
+unique_values = list(set(numbers))
+print("Unique values:", unique_values)
+
