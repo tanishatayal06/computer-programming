8265ac8afa8beee614da86afdf55812222828a97
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-27 14:23:25 +0530

    git push origin main

---
+++ coding-assignment/list/list_q16.py
@@ -0,0 +1,6 @@
+# Problem: Write a Python program to shuffle and print a specified list (shuffle)
+import random
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+random.shuffle(numbers)
+print("Shuffled list:", numbers)
+
