0afca9ca28409b31ed5489d29a6926a9bfa52430
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-27 14:32:04 +0530

    git push origin main

---
+++ coding-assignment/list/list_q20.py
@@ -0,0 +1,7 @@
+# Problem: Create a list by concatenating a given list which range goes from 1 to n
+original_list = [10, 20, 30]
+n = 5
+range_list = list(range(1, n + 1))
+concatenated_list = original_list + range_list
+print("Concatenated List:", concatenated_list)
+
