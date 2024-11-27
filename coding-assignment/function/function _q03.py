c645e15c9992d62be6ac1694cd399c57042adf97
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-27 09:57:15 +0530

    git push origin main

---
+++ coding-assignment/function/function _q03.py
@@ -0,0 +1,16 @@
+# Problem: Given a list of intervals, merge all overlapping intervals and return the resulting list.
+def merge_intervals(intervals):
+    intervals.sort(key=lambda x: x[0])
+    merged = []
+    
+    for interval in intervals:
+        if not merged or merged[-1][1] < interval[0]:
+            merged.append(interval)
+        else:
+            merged[-1][1] = max(merged[-1][1], interval[1])
+    
+    return merged
+
+intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+print(merge_intervals(intervals))  # Output: [[1, 6], [8, 10], [15, 18]]
+
