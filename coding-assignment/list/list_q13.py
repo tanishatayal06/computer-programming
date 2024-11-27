d6ce5d6de8fdc49b8093850147eb92aab30b2a4a
tanishatayal06 <tanishatayal2007@gmail.com>
2024-11-27 09:41:01 +0530

    git push origin main

---
+++ coding-assignment/list/list_q13.py
@@ -0,0 +1,6 @@
+# Problem: Get the items from a list starting at position start and ending at position end.
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+start=int(input())
+end=int(input())
+print(a[start:end])
+
